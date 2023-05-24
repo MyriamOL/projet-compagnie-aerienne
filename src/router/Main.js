9685c9493b9126ad91d37a9ab0c97187363fe828
PierreAnatole <pierranatole@free.fr>
2023-05-24 11:10:18 +0200

    merge

--- src/router/Main.js
+++ src/router/Main.js
@@ -4,11 +4,8 @@ import Accueil from "../views/Accueil";
 import QuiSommesNous from "../views/QuiSommesNous";
 import ListeResultats from "../views/ListeResultats";
 import ListeAeroports from "../views/ListeAeroports";
-<<<<<<< HEAD
 import BilletsVoles from "../views/BilletsVoles";
-=======
 import ListeVols from "../views/ListeVols";
->>>>>>> 2267fe8151dbf13d5a0a20f79ad164a927bb9748
 
 export default function Main() {
   return (
@@ -17,11 +14,8 @@ export default function Main() {
       <Route path="/qui-sommes-nous" element={<QuiSommesNous />} />
       <Route path="/liste-resultats" element={<ListeResultats />} />
       <Route path="/liste-aeroports" element={<ListeAeroports />} />
-<<<<<<< HEAD
       <Route path="/billets-voles" element={<BilletsVoles />} />
-=======
       <Route path="/liste-vols" element={<ListeVols />} />
->>>>>>> 2267fe8151dbf13d5a0a20f79ad164a927bb9748
     </Routes>
   );
 }
