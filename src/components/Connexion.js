ff7f3070212bd42622735a8ef92d75545ee95b23
PierreAnatole <pierranatole@free.fr>
2023-05-24 11:50:50 +0200

    ajout auth react

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,19 +1,61 @@
+import { useSignIn } from "react-auth-kit";
 import "../styles/components/formulaire.css";
+import axios from "axios";
+
+import { useState } from "react";
 
 export default function Connexion({ goToCreationCompte, goToRecuperationMdp }) {
+  /* connexion */
+  const signIn = useSignIn();
+  const apiUrl = "http://localhost:8080/connexion";
+
+
+  /* formulaire */
+  const [email, setEmail] = useState('');
+  const [mdp, setMdp] = useState('');
+
+  /* fonctions formulaire */
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleChangeMdp(e) {
+    setMdp(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    axios.post(apiUrl, {
+      email:email,
+      mdp:mdp
+    })
+    .then((response) => {
+        console.log(response.data);
+        console.log(response.data.jwttoken);
+        signIn({
+          token: response.data.jwttoken,
+          tokenType: "Bearer",
+          authState:email
+        })
+    })
+
+
+  }
+
   return (
     <main id="formulaire">
       <div className="form-wrapper">
         <h2>Connexion</h2>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-text-field">
             <label htmlFor="email">Adresse e-mail</label>
-            <input type="text" id="email" name="email" />
+            <input type="text" id="email" name="email" onChange={handleChangeEmail}/>
           </div>
           <div className="input-text-field">
             <label htmlFor="mdp">Mot de passe (6 charactères minimum)</label>
-            <input type="password" minLength="6" id="mdp" name="mdp" />
+            <input type="password" minLength="6" id="mdp" name="mdp" onChange={handleChangeMdp}/>
           </div>
 
           {/* <span className="form-link-nav" onClick={goToRecuperationMdp}>
--- src/index.js
+++ src/index.js
@@ -7,15 +7,23 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store/store';
+import { AuthProvider } from "react-auth-kit";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <AuthProvider
+        authType={"cookie"}
+        authName={"_auth"}
+        cookieDomain={window.location.hostname}
+        cookieSecure={false}
+      >
       <BrowserRouter>
         <App />
       </BrowserRouter>
+      </AuthProvider>
     </Provider>
   </React.StrictMode>
 );
