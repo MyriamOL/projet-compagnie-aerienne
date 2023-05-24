9e6347cc9402185b58fef37ac5ee230f7ff9c055
Myriam ould lazazi <ouldlazazimimi@gmail.com>
2023-05-24 14:19:40 +0200

    supp liste vole et list aeroport

--- src/router/Main.js
+++ src/router/Main.js
@@ -3,9 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Accueil from "../views/Accueil";
 import QuiSommesNous from "../views/QuiSommesNous";
 import ListeResultats from "../views/ListeResultats";
-import ListeAeroports from "../views/ListeAeroports";
-import ListeVols from "../views/ListeVols";
-
 
 
 export default function Main() {
@@ -14,13 +11,6 @@ export default function Main() {
       <Route index element={<Accueil />} />
       <Route path="/qui-sommes-nous" element={<QuiSommesNous />} />
       <Route path="/liste-resultats" element={<ListeResultats />} />
-      <Route path="/liste-aeroports" element={<ListeAeroports />} />
-      <Route path="/liste-vols" element={<ListeVols />} />
-      <Route path="/list-aeroports" element={<ListeAeroports />}  />
-      
-
-      
-
     </Routes>
   );
 }
